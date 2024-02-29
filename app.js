cd6d15f5b2e2deeae867bdea9ea7011fd01b6cdb
muhammad-danish <totalnetsolution2@gmail.com>
2024-03-01 04:27:28 +0500

    index file created

---
+++ app.js
@@ -0,0 +1,184 @@
+// console.log('hello world');
+
+// var userName = 'abdullah';
+// console.log(userName)
+
+
+// alert('hello world');
+// alert('hello world');
+
+
+// var num1 = 10;
+// var num2 = '20';
+// var num3 = 20;
+
+// console.log(typeof(num1))
+// console.log(num1 + num2 + num3)
+
+
+
+
+
+// var username = prompt('Enter your name!');
+// console.log(username);
+
+
+
+
+
+// var num1 = Number(prompt('Enter first number'));
+// var num2 = Number(prompt('Enter second number'));
+
+
+// var num1 = +prompt('Enter first number');
+// var num2 = +prompt('Enter second number');
+// console.log(num1 + num2);
+
+
+
+
+
+//Arithmatic Operator
+
+// +
+// -
+// *
+// /
+// % == modulus
+
+// var num1 = 10;
+// var num2 = 5;
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log('modulus ===> ', num1 % num2);
+// console.log(10 + 20 - 10 * 5 / 2);
+
+
+
+
+
+
+
+
+
+
+
+
+//Assignment Operator
+// =
+// +=
+// -=
+// /=
+// *=
+
+// var num1 = 12;
+// num1 = num1 + 10;
+// num1 += 10;
+// num1 -= 10;
+// num1 *= 10;
+// num1 /= 10;
+// console.log(num1);
+
+
+
+//Comparision Operator
+
+// >
+// <
+// == || !=
+// === || !==
+// != data type check nahi kraiga!
+// !== data type check kraiga!
+// >=
+// <=
+
+// console.log(10 == '10');
+// console.log(10 === '10');
+
+// console.log(10 != 10);
+// console.log(10 == '10');
+// var username = 'abdullah'
+// document.write('<h1>'+ username+ '</h1>');
+
+
+
+
+//logical Operator
+
+//&& == and
+//|| == OR
+
+
+// var comparision = 10 <= 10 || 10 < 6 || 10 > 15;
+// var comparision = 10 <= 10 && 10 < 6 && 10 > 15;
+// console.log(comparision);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// num++ post increment
+// ++num pre increment 
+// var num = 10;
+// console.log(num + 1);
+// console.log(++num);
+// console.log(num++);
+// console.log(num++)
+// console.log(++num)
+
+
+
+
+
+// var num = 10
+// var calculation = ++num + num++ + ++num;
+// 11 + 11 + 13
+// var calculation = ++num + num++ + ++num + --num + num-- + num++ + ++num;
+// //11 + 11 + 13 + 12 + 12 + 11 + 13;
+
+
+// console.log(calculation , 11 + 11 + 13 + 12 + 12 + 11 + 13);
+
+
+
+
+
+
+
+
+
+
+
+
+
+var num; prompt("Please Enter any number");
+if(num % 2 == 0){
+    console.log('number is divisible by 2');
+}else{
+    console.log('number is not divisible by 2');
+}
